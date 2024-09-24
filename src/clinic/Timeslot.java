3452d4642dd3c2e43986bd0f4e41ae1c21ef2934
scarletrat <gl548@scarletmail.rutgers.edu>
2024-09-24 17:55:48 -0400

    Updated timeslot class commit

--- src/clinic/Timeslot.java
+++ src/clinic/Timeslot.java
@@ -1,4 +1,18 @@
 package clinic;
 
+/**
+ * This class defines the time slots of a day in the format HH:MM
+ *
+ */
 public enum Timeslot {
+    SLOT1 (9, 0),
+    SLOT2 (10, 45),
+    SLOT3 (11, 15),
+    SLOT4 (13, 30),
+    SLOT5 (15, 0),
+    SLOT6 (16, 15);
+
+    private Timeslot(int hour, int minute) {
+    }
+
 }
