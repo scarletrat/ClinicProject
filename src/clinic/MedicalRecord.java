7ea00a3c53456c575627fd0c807cc63c38840c3a
scarletrat <gl548@scarletmail.rutgers.edu>
2024-09-29 00:41:11 -0400

    Starting MedicalRecord Commit

--- src/clinic/MedicalRecord.java
+++ src/clinic/MedicalRecord.java
@@ -1,4 +1,18 @@
 package clinic;
 
 public class MedicalRecord {
+    private Patient[] patients;
+    private int size;
+
+    /**
+     * One argument constructor.
+     * @param patient tne patient to put inside the record.
+     */
+    public MedicalRecord(Patient patient){
+        patients = new Patient[1];
+        patients[0] = patient;
+        size = size + 1;
+    }
+
+
 }
