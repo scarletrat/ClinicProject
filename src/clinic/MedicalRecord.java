0a9f423cd56a85a5444a5292b86346f2e86e9bc9
scarletrat <gl548@scarletmail.rutgers.edu>
2024-09-30 10:38:57 -0400

    Scheduler and MedicalRecord commit

--- src/clinic/MedicalRecord.java
+++ src/clinic/MedicalRecord.java
@@ -9,39 +9,94 @@ public class MedicalRecord {
     private Patient[] patients;
     private int size;
 
+    public MedicalRecord(){
+        patients = null;
+        size = 0;
+    }
+
     /**
-     * One argument constructor.
-     * @param patient tne patient to put inside the record.
+     * Add a new patient to medical record.
+     * @param appointment the patient's appointment.
      */
-    public MedicalRecord(Patient patient){
-        patients = new Patient[1];
-        patients[0] = patient;
-        size = size + 1;
+    public void addNewPatient(Appointment appointment){
+        Patient newPatient = new Patient(appointment.getProfile(), appointment);
+        Patient [] temp = new Patient[size+1];
+        for(int i =0; i<size; i++){
+            temp[i] = patients[i];
+        }
+        temp[size] = newPatient;
+        patients = temp;
+    }
+
+    /**
+     * Get the size of medical record.
+     * @return return the size of medical record.
+     */
+    public int getSize(){
+        return size;
     }
 
     /**
      * Add a patient to the medical record.
-     * @param patient the patient to be added.
+     * @param clinic the clinic containing all the appointments.
      */
-    public void addPatient(Patient patient){
-        Patient[] temp = new Patient[size+1];
-        for(int i=0; i< size; i++){
-            temp[i] = patients[i];
+    public void add(List clinic){
+        int size = clinic.getSize();
+        Appointment[] appointments = clinic.getAppointment();
+        for(int i = 0; i<size; i++){
+            Profile profile = appointments[i].getProfile();
+            if(contains(profile)){
+                Patient patient = findPatient(profile);
+                updatePatient(patient,appointments[i]);
+            }else{
+                addNewPatient(appointments[i]);
+            }
         }
-        patients = temp;
-        size += 1;
+    }
+
+    /**
+     * Find the patient in medical record.
+     * @param profile the patient's profile
+     * @return return the patient.
+     */
+    public Patient findPatient(Profile profile){
+        for(int i = 0; i< size; i++){
+            if(patients[i].getProfile().equals(profile)){
+                return patients[i];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Check if the medical record contains the profile.
+     * @param profile the profile to be checked.
+     * @return return true if there's a record of the profile.
+     * return false otherwise.
+     */
+    public boolean contains(Profile profile){
+        for(int i = 0; i< size; i++){
+            if(patients[i].getProfile().equals(profile)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Get the patients array of medical record.
+     * @return return the patients array.
+     */
+    public Patient[] getPatients(){
+        return patients;
     }
 
     /**
      * Updates the patient's profile.
-     * @param patient the patient to be updated.
+     * @param appointment the patient's profile to be updated.
      */
-    public void updatePatient(Patient patient){
-        for(int i = 0; i<size; i++){
-            if (patients[i].getProfile().equals(patient.getProfile())){
-                patients[i] = patient;
-            }
-        }
+    public void updatePatient(Patient patient,Appointment appointment){
+        patient.addVisit(appointment);
     }
 
 }
