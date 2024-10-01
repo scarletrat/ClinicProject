a4fb62774b05de2834b0cb710d87c9797df7f93b
scarletrat <gl548@scarletmail.rutgers.edu>
2024-09-30 20:02:12 -0400

    commit

--- src/clinic/RunProject1.java
+++ src/clinic/RunProject1.java
@@ -2,6 +2,7 @@ package clinic;
 
 /** A driver to test run Project 1
  * Use Scheduler().run();
+ * @author Gordon Lin, modified 9/30/2024.
  */
 public class RunProject1 {
     public static void main(String[] args){
